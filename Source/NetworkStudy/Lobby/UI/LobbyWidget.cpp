1f2ed7b66f6e8637e3bd8b3ea6a7347cb590693d
BATMINJI <minjiconan@naver.com>
2026-05-26 14:45:21 +0900

    Lobby Widget GetLobbyPlayerStates, GetLobbyPlayerStateAtIndex 함수 CPP화

--- Source/NetworkStudy/Lobby/UI/LobbyWidget.cpp
+++ Source/NetworkStudy/Lobby/UI/LobbyWidget.cpp
@@ -2,4 +2,37 @@
 
 
 #include "Lobby/UI/LobbyWidget.h"
+#include "Lobby/UI/LobbyUserWidget.h"
+#include "Lobby/Contents/LobbyPlayerState.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/GameStateBase.h"
 
+TArray<ALobbyPlayerState*> ULobbyWidget::GetLobbyPlayerStates()
+{
+	TArray<ALobbyPlayerState*> PlayerStates;
+
+	AGameStateBase* GateStateBase = UGameplayStatics::GetGameState(this);
+	if (GateStateBase)
+	{
+		for (APlayerState* PlayerState : GateStateBase->PlayerArray)
+		{
+			ALobbyPlayerState* LobbyPlayerState = Cast<ALobbyPlayerState>(PlayerState);
+			if (IsValid(LobbyPlayerState))
+			{
+				PlayerStates.Add(LobbyPlayerState);
+			}
+		}
+	}
+
+	return PlayerStates;
+}
+
+ALobbyPlayerState* ULobbyWidget::GetLobbyPlayerStateAtIndex(int32 InIndex)
+{
+	TArray<ALobbyPlayerState*> PlayerStates = GetLobbyPlayerStates();
+	if (PlayerStates.IsValidIndex(InIndex))
+	{
+		return PlayerStates[InIndex];
+	}
+	return nullptr;
+}
--- Source/NetworkStudy/Lobby/UI/LobbyWidget.h
+++ Source/NetworkStudy/Lobby/UI/LobbyWidget.h
@@ -6,12 +6,37 @@
 #include "Blueprint/UserWidget.h"
 #include "LobbyWidget.generated.h"
 
-/**
- * 
- */
+class UVerticalBox;
+class UButton;
+class ULobbyUserWidget;
+class ALobbyPlayerState;
+
 UCLASS()
 class NETWORKSTUDY_API ULobbyWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	TArray<ALobbyPlayerState*> GetLobbyPlayerStates();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	ALobbyPlayerState* GetLobbyPlayerStateAtIndex(int32 InIndex);
+
+protected:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TObjectPtr<UVerticalBox> UserList;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TObjectPtr<UButton> Btn_Close;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TObjectPtr<UButton> Btn_GameConfig;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<ULobbyUserWidget> LobbyUserWidgetClass;
+
+	UPROPERTY(BlueprintReadWrite)
+	TArray<TObjectPtr<ULobbyUserWidget>> LobbyUsers;
 };
